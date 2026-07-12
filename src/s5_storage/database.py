f9195264a2b974ebeb396143edd2ca95e1da3271
Yu <ge75qeb@mytum.de>
2026-07-12 19:31:28 +0900

    feat: created database schmea for images, stats and outliers

--- src/s5_storage/database.py
+++ src/s5_storage/database.py
@@ -1,2 +1,32 @@
-#TODO
-#database
+from sqlalchemy import Table, Column, create_engine, Table, Integer, Float, Text, ForeignKey, MetaData
+
+engine = create_engine("sqllite:///pipeline.db")
+metadata = MetaData()
+
+images = Table(
+  "images", metadata,   
+  Column("id", Integer, primary_key=True),
+  Column("filename", Text, unique=True),
+  Column("width", Integer),
+  Column("height", Integer)
+  )
+
+stats = Table("stats", metadata,
+  Column("image_id", Integer, ForeignKey("images.id")),
+  Column("brightness", Float),
+  Column("contrast", Float),
+  Column("blur_score", Float)
+  )
+
+outliers = Table("outliers", metadata,
+  Column("image_id", Integer, ForeignKey("images.id")),
+  Column("method", Text),
+  Column("metric", Text),
+  Column("score", Float)
+  )
+
+
+def create_all_tables():
+  metadata.create_all(engine)
+  print("tables created in pipeline.db")
+
