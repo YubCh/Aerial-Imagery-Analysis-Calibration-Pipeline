7f5aa31905d899597c07f250193c6dee1f861ff9
Yu <ge75qeb@mytum.de>
2026-07-27 22:45:15 +0900

    feat: add pretrained model

--- src/s3_deep_learning_features/image_classification.py
+++ src/s3_deep_learning_features/image_classification.py
@@ -1,2 +1,16 @@
-#TODO
-#run images through the neural net
+import cv2
+import numpy as np
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+
+device = "mps" if torch.backends.mps.is_available else "cpu"
+
+
+def load_model():
+  model = models.resnet18(weights="IMAGET1K_V1")
+  model.eval()
+  model.to(device)
+  print(f"model ready to use on {device}")
+  return model
